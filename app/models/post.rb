8e6dcfbc3772ec6a938eb456d3dc627e17ccbf3d
Greg Garville Jr <gjgarvil@gmail.com>
2017-11-01 20:56:40 -0400

    fixed syntax error in post controller after testing on local server

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_attached_file :avatar, styles: { medium: "700x500#", small: "350x250>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, styles: { medium: "700x500#", small: "350x250#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end

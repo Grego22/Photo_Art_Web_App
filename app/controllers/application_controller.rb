dc9cad8182acac37246d146765cc0c190bbe63a3
Greg Garville Jr <gjgarvil@gmail.com>
2017-11-01 20:00:01 -0400

    fixed syntax errors in application controller after testing on local server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   	protected
 
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   	  devise_parameter_sanitizer.for(:sign_up) << :name
   	  devise_parameter_sanitizer.for(:account_update) << :name
 
-
+    end
 
 end
